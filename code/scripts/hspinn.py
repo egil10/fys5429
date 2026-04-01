d2037951816d53d9e0930434322268d433c45174
egil10 <egilfure@gmail.com>
2026-04-01 08:55:22 +0200

    hspinn and train_hs

--- code/scripts/hspinn.py
+++ code/scripts/hspinn.py
@@ -3,14 +3,15 @@ import torch.nn as nn
 
 
 class HSPINN(nn.Module):
-    def __init__(self, hidden_layers=3, neurons_per_layer=128,
-                 S_scale=300.0, tau_scale=1.0, activation='softplus'):
+    def __init__(self, hidden_layers=4, neurons_per_layer=256,
+                 S_scale=300.0, v_scale=1.0, tau_scale=1.0, activation='tanh'):
         super().__init__()
 
         self.S_scale = S_scale
+        self.v_scale = v_scale
         self.tau_scale = tau_scale
 
-        layers = [nn.Linear(2, neurons_per_layer),
+        layers = [nn.Linear(3, neurons_per_layer),
                   nn.Softplus() if activation == 'softplus' else nn.Tanh()]
         for _ in range(hidden_layers):
             layers.append(nn.Linear(neurons_per_layer, neurons_per_layer))
@@ -26,8 +27,9 @@ class HSPINN(nn.Module):
                 nn.init.xavier_normal_(m.weight)
                 nn.init.zeros_(m.bias)
 
-    def forward(self, S, tau):
+    def forward(self, S, v, tau):
         S_norm = S / self.S_scale
+        v_norm = v / self.v_scale
         tau_norm = tau / self.tau_scale
-        x = torch.cat([S_norm, tau_norm], dim=1)
+        x = torch.cat([S_norm, v_norm, tau_norm], dim=1)
         return self.net(x)
--- code/scripts/train_hs.py
+++ code/scripts/train_hs.py
@@ -4,12 +4,15 @@ import torch.optim as optim
 from hspinn import HSPINN
 
 
-def train_pinn(S_in, tau_in, S_ic, tau_ic, S_bc, tau_bc,
-               sigma, r, K, device,
+def train_pinn(S_in, v_in, tau_in,
+               S_ic, v_ic, tau_ic,
+               S_bc, v_bc, tau_bc,
+               r, K, kappa, theta, xi, rho,
+               device,
                lambda_pde, lambda_ic, lambda_bc, epochs,
-               lr=0.001, hidden_layers=3, neurons=128, activation='softplus'):
+               lr=0.001, hidden_layers=4, neurons=256, activation='tanh'):
 
-    model = BSPINN(hidden_layers, neurons, activation=activation).to(device)
+    model = HSPINN(hidden_layers, neurons, activation=activation).to(device)
     optimizer = optim.Adam(model.parameters(), lr=lr)
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs, eta_min=1e-6)
 
@@ -20,21 +23,41 @@ def train_pinn(S_in, tau_in, S_ic, tau_ic, S_bc, tau_bc,
     for epoch in range(epochs):
         optimizer.zero_grad()
 
-        V_pred = model(S_in, tau_in)
-        V_S = torch.autograd.grad(V_pred, S_in, grad_outputs=grad_ones, create_graph=True)[0]
-        V_tau = torch.autograd.grad(V_pred, tau_in, grad_outputs=grad_ones, create_graph=True)[0]
-        V_SS = torch.autograd.grad(V_S, S_in, grad_outputs=grad_ones, create_graph=True)[0]
+        # --- PDE residual ---
+        V_pred = model(S_in, v_in, tau_in)
 
-        pde_residual = V_tau - (0.5 * sigma**2 * S_in**2 * V_SS + r * S_in * V_S - r * V_pred)
+        # First-order derivatives
+        V_S   = torch.autograd.grad(V_pred, S_in,   grad_outputs=grad_ones, create_graph=True)[0]
+        V_v   = torch.autograd.grad(V_pred, v_in,   grad_outputs=grad_ones, create_graph=True)[0]
+        V_tau = torch.autograd.grad(V_pred, tau_in,  grad_outputs=grad_ones, create_graph=True)[0]
+
+        # Second-order derivatives
+        V_SS  = torch.autograd.grad(V_S, S_in, grad_outputs=grad_ones, create_graph=True)[0]
+        V_vv  = torch.autograd.grad(V_v, v_in, grad_outputs=grad_ones, create_graph=True)[0]
+        V_Sv  = torch.autograd.grad(V_S, v_in, grad_outputs=grad_ones, create_graph=True)[0]
+
+        # Heston PDE: V_tau = 0.5*v*S^2*V_SS + rho*xi*v*S*V_Sv + 0.5*xi^2*v*V_vv
+        #                     + r*S*V_S + kappa*(theta-v)*V_v - r*V
+        pde_residual = V_tau - (
+            0.5 * v_in * S_in**2 * V_SS
+            + rho * xi * v_in * S_in * V_Sv
+            + 0.5 * xi**2 * v_in * V_vv
+            + r * S_in * V_S
+            + kappa * (theta - v_in) * V_v
+            - r * V_pred
+        )
         loss_pde = torch.mean(pde_residual**2)
 
-        V_ic_pred = model(S_ic, tau_ic)
+        # --- IC loss (tau=0): payoff = max(S - K, 0) ---
+        V_ic_pred = model(S_ic, v_ic, tau_ic)
         V_ic_true = torch.relu(S_ic - K)
         loss_ic = torch.mean((V_ic_pred - V_ic_true)**2)
 
-        V_bc_pred = model(S_bc, tau_bc)
+        # --- BC loss (S=0): V(0, v, tau) = 0 ---
+        V_bc_pred = model(S_bc, v_bc, tau_bc)
         loss_bc = torch.mean((V_bc_pred - 0.0)**2)
 
+        # --- Total loss ---
         loss = lambda_pde * loss_pde + lambda_ic * loss_ic + lambda_bc * loss_bc
         loss.backward()
         torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
